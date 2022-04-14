eda0c9bcc0f9723877068721056e50ff7e73bd0a
Quentin Motte <motte.quentin1@gmail.com>
2022-04-14 10:56:15 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //For the sidebar
 const sideBar = document.querySelector(".sideBar");
 const btn = document.getElementById("btn");
+const hamburger = document.querySelector(".side-bar");
 const content = document.querySelector("main");
 
 //For the slides
@@ -9,42 +10,65 @@ const btnNumberTwo = document.getElementById("slide2");
 const btnNumberThree = document.getElementById("slide3");
 const btnNumberFour = document.getElementById("slide4");
 const sliderForTranslateOnX = document.querySelector(".sliders");
-let largeur = window.innerWidth;
-let playOnce = true;
 
-//sidebar Logic
-btn.addEventListener("click", () => {
-    sideBar.classList.toggle("active");
-});
-content.addEventListener("click", () => {
-    sideBar.classList.remove("active");
-});
+//slider when u arrive on the page
 
-btnNumberOne.addEventListener("click", () => {
-    if (largeur >= 1116) {
-        sliderForTranslateOnX.style.transform = "translateX(" + 420 + "px)";
-    } else if (largeur <= 1115) {
-        sliderForTranslateOnX.style.transform = "translateX(" + 0 + "px)";
+newWidth = window.innerWidth;
+
+function widthTranslate(translate1, translate2) {
+    if (newWidth >= 1116) {
+        sliderForTranslateOnX.style.transform =
+            "translateX(" + translate1 + "px)";
+    } else if (newWidth <= 1115) {
+        sliderForTranslateOnX.style.transform =
+            "translateX(" + translate2 + "px)";
     }
+}
+btnNumberOne.addEventListener("click", () => {
+    widthTranslate(420, 0);
 });
 btnNumberTwo.addEventListener("click", () => {
-    if (largeur >= 1116) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -150 + "px)";
-    } else if (largeur <= 1115) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -380 + "px)";
-    }
+    widthTranslate(-150, -380);
 });
 btnNumberThree.addEventListener("click", () => {
-    if (largeur >= 1116) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -710 + "px)";
-    } else if (largeur <= 1115) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -750 + "px)";
-    }
+    widthTranslate(-710, -750);
 });
 btnNumberFour.addEventListener("click", () => {
-    if (largeur >= 1116) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -1300 + "px)";
-    } else if (largeur <= 1115) {
-        sliderForTranslateOnX.style.transform = "translateX(" + -1140 + "px)";
-    }
+    widthTranslate(-1300, -1140);
 });
+// This is for when u change the witdh
+
+window.addEventListener("resize", function (event) {
+    var newWidth = window.innerWidth;
+    function widthTranslate(translate1, translate2) {
+        if (newWidth >= 1116) {
+            sliderForTranslateOnX.style.transform =
+                "translateX(" + translate1 + "px)";
+        } else if (newWidth <= 1115) {
+            sliderForTranslateOnX.style.transform =
+                "translateX(" + translate2 + "px)";
+        }
+    }
+    console.log(newWidth);
+    btnNumberOne.addEventListener("click", () => {
+        widthTranslate(420, 0);
+    });
+    btnNumberTwo.addEventListener("click", () => {
+        widthTranslate(-150, -380);
+    });
+    btnNumberThree.addEventListener("click", () => {
+        widthTranslate(-710, -750);
+    });
+    btnNumberFour.addEventListener("click", () => {
+        widthTranslate(-1300, -1140);
+    });
+});
+
+// sidebar Logic
+btn.addEventListener("click", () => {
+    sideBar.classList.toggle("active");
+    hamburger.classList.toggle("active");
+});
+// content.addEventListener("click", () => {
+//     sideBar.classList.remove("active");
+// });
